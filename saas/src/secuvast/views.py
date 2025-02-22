4abdc9d5a7b19bd124e6b4bc8ed2144463784dca
Edgar <edgar.jgg93@gmail.com>
2025-02-22 02:55:45 +0000

    update

--- saas/src/secuvast/views.py
+++ saas/src/secuvast/views.py
@@ -6,14 +6,22 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all() # everything saved
     page_qs = PageVisit.objects.filter(path=request.path)
     my_tittle = "Secuvast"
+    try:
+        percent =  (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_context = {
         "page_tittle": my_tittle,
         "page_visit_count": page_qs.count(),
-        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count(),
         
     }
